007bc22d9ef12f1bcc2afefb50f1730cec64e869
Taigaraider <taigatimothy5@gmail.com>
2026-07-28 16:43:02 +0100

    fix: restore rewrites in vercel.json, simplify handler

--- api/index.js
+++ api/index.js
@@ -1,6 +1,3 @@
 import app from "../server/src/index.js";
 
-export default function handler(req, res) {
-  req.url = req.url.replace(/^\/api/, "") || "/";
-  return app(req, res);
-}
+export default app;
