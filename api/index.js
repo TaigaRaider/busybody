ddc3a7efed7fa9a7615e0614a8ce208562386d5a
Taigaraider <taigatimothy5@gmail.com>
2026-07-28 16:49:58 +0100

    fix: wrap import in try-catch for debugging

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
-import app from "../server/src/index.js";
+let app;
+try {
+  app = (await import("../server/src/index.js")).default;
+} catch (e) {
+  console.error("Init error:", e);
+  app = (req, res) => {
+    res.status(500).json({ error: "Init failed", message: e.message });
+  };
+}
 
 export default app;
