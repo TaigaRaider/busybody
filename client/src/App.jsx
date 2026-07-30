769bcdd6e2e804d8d1aba71fff3f49880549f9ea
Taigaraider <taigatimothy5@gmail.com>
2026-07-30 16:14:01 +0100

    Enable Vercel Web Speed Insights for react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { useState, useEffect } from "react";
 import {
   fetchNotes,
